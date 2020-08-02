52ff1730670b5e14541c76b0c3461c66b3ef2a07
Uibiran <32081339+Uibiran@users.noreply.github.com>
2020-08-01 22:57:09 -0300

    terceiro commit ( movimento )

--- script.js
+++ script.js
@@ -35,8 +35,11 @@ function iniciarJogo(){
     snake.pop();
 
     let newHead = {
-
+        x: snakeX,
+        y: snakeY
     }
+
+    snake.unshift(newHead);
 }
 
 let jogo = setInterval(iniciarJogo, 100);
