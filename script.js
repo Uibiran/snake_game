65e5ba2e8527f18355de369d829258860e29ca61
Uibiran <32081339+Uibiran@users.noreply.github.com>
2020-08-01 22:56:40 -0300

    terceiro commit ( movimento )

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ snake[0] = {
     x: 4 * box,
     y: 4 * box
 }
+let direction = "right";
 
 function criarBG(){
     context.fillStyle = "lightblue";
-    context.fillRect(0, 0, 32 * box, 32 * box);
+    context.fillRect(0, 0, 64 * box, 64 * box);
 }
 
 function criarCobrinha(){
@@ -19,5 +20,23 @@ function criarCobrinha(){
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+
+    }
+}
+
+let jogo = setInterval(iniciarJogo, 100);
