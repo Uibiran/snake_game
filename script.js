e8e444ab5e983007a5ca57565c68775d1928e395
Uibiran <32081339+Uibiran@users.noreply.github.com>
2020-08-02 22:37:59 -0300

    Update script.js
    definindo surgimento aleatório das frutas e crescimento da cobrinha.

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ snake[0] = {
     y: 4 * box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 31 + 1) * box,
+    y: Math.floor(Math.random() * 31 + 1) * box
+}
 
 function criarBG(){
     context.fillStyle = "lightblue";
@@ -18,6 +22,11 @@ function criarCobrinha(){
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function drawFood(){
+    context.fillStyle = "purple";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -38,6 +47,7 @@ function iniciarJogo(){
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -47,7 +57,12 @@ function iniciarJogo(){
     if (direction == "up") snakeY -= box;
     if (direction == "down") snakeY += box;
 
-    snake.pop();
+    if (snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }else{
+        food.x = Math.floor(Math.random() * 31 + 1) * box;
+        food.y = Math.floor(Math.random() * 31 + 1) * box;
+    }
 
     let newHead = {
         x: snakeX,
